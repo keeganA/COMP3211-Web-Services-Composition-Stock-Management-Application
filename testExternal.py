5af202eeeaf8b7f92b8a39b2c58ca0c622b27690
keegan <ed19kja@leeds.ac.uk>
2022-11-22 18:19:25 +0000

    external api client addition

--- testExternal.py
+++ testExternal.py
@@ -6,3 +6,11 @@ stock = yf.Ticker("Meta")
 stockInfo = stock.info
 end=time()
 print(end-start)
+
+
+def GetSummaryAndPrice():
+        stockInput = input("Which stock would you want go get to know more about. Please enter the ticker symbol of the stock?")
+        stock = yf.Ticker(stockInput)
+        stockInfo = stock.info
+        print("\nHere is some information about the stock: ", stockInfo["longBusinessSummary"])
+        print("\nThe current price of the stock is: ", stockInfo["regularMarketPrice"])
